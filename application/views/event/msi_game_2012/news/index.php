e3a491f46ca9c257889e72116de46a9f28343db9
lzzpnk <aiyswu@gmail.com>
2012-07-18 03:00:11 +0800

    調整部份切版

--- application/views/event/msi_game_2012/news/index.php
+++ application/views/event/msi_game_2012/news/index.php
@@ -1,8 +1,11 @@
-<?php foreach ($news_flow as $key => $news): ?>
+<div class="well">
+	<?php foreach ($news_flow as $key => $news): ?>
 	<div>
+		<span class="label label-info">[<?php echo date('Y-m-d', $news['dateline']) ?>]</span>
 		<?php echo $this->discuzx->alink_to_bbs( array(
 			'tid'  => $news['tid'],
 			'text' => $news['subject'],
 		) ) ?>
 	</div>
-<?php endforeach ?>
+	<?php endforeach ?>
+</div>
