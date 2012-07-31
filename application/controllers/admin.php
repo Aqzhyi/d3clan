f36a4c9feef2aaddb33f13829ffb7741e7babe6f
lzzpnk <aiyswu@gmail.com>
2012-08-01 06:07:50 +0800

    /admin: 以smarty視圖引擎重構

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,31 +1,35 @@
 <?php if ( ! defined( 'BASEPATH' ) ) exit( 'No direct script access allowed' );
 
-class admin extends CI_Controller {
+class Admin extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
 		$this->load->helper( 'form' );
+		$this->load->library( 'Json' );
+		$this->view->layout( 'admin/layout' );
+		$this->view->css_add( 'admin/diy' );
 		$this->view->cache( 0 );
+
+		// if ( ! $this->user->auth( 21 ) ) show_404(); // 直播頻道管理大師
+		// if ( ! $this->user->auth( 22 ) ) show_404(); // 網站首頁管理大師
+		// if ( ! $this->user->auth( 23 ) ) show_404(); // OCR封閉測試
+	}
+
+	public function _remap( $method = 'index', $params = array() ) {
+		$this->view->title_routes( array(
+				'index'         => '通用後台管理頁面',
+				'live_channels' => '直播頻道管理',
+			) );
+		$this->view->page( $method, $params );
+		$this->view->init( $this );
 	}
 
 	public function index() {
-		if ( ! $this->user->auth( 22 ) ) {
-			show_404();
-		}
 
-		$this->view->display(
-			array(
-				'title'    => "通用後台管理頁面",
-				'view'     => 'admin/index',
-				'js_files' => array(
-					'admin/index',
-				),
-				'css_files' => array(
-					'admin/diy',
-					'admin/index',
-				),
-			)
-		);
+		if ( ! $this->user->auth( 22 ) ) show_404();
+
+		$this->view->js_add( 'admin/index' );
+		$this->view->css_add( 'admin/index' );
 	}
 
 	public function live_channels() {
@@ -34,121 +38,8 @@ class admin extends CI_Controller {
 				'limit' => 200,
 				'game_type' => 'DiabloIII',
 			) );
-		$this->view->display(
-			array(
-				'title'    => "直播頻道管理",
-				'view'     => 'admin/live-channels',
-				'js_files' => array(
-					'admin/live-channels',
-				),
-				'css_files' => array(
-					'admin/diy',
-					'admin/live-channels'
-				),
-			)
-		);
-	}
-
-	public function news_bot( $type = '', $data = array() ) {
-		switch ( $type ) {
-		case 'latest-vod':
-			$this->_fetch_latest_vod();
-			break;
-		default:
-			if ( is_array( $type ) ) {
-				$data = $type;
-			}
-
-			foreach ( $data as $key => $value ) {
-				$this->view->data[$key] = $value;
-			}
-
-			$this->view->display(
-				array(
-					'title'    => "網易新聞爬蟲",
-					'view'     => 'admin/news-bot',
-					'js_files' => array(
-						'admin/news-bot',
-					),
-					'css_files' => array(
-						'admin/diy',
-						'admin/news-bot'
-					),
-				)
-			);
-			break;
-		}
-	}
-
-	/**
-	 * 擷取最新視頻
-	 * 拿最近新增進論壇的60筆，比對網易最新的15筆。
-	 *
-	 * @param array   $setting [description]
-	 * @return [type]          [description]
-	 */
-	private function _fetch_latest_vod( $setting = array() ) {
-		$this->load->library( 'simple_html_dom' );
-		$this->load->model( 'Model_vod' );
-		// require_once FCPATH . 'extention/big2gb/big2gb.php';
-		$html = file_get_html( "http://d.163.com/special/new_shipin/" );
-		// $code = new big2gb;
-
-		// 載入已轉過的vod
-		$exisit_vod = $this->Model_vod->get_vod( array(
-				'limit' => 60,
-				'offset' => 0,
-			) );
-
-		$fetch_rows = array();
-
-		// 列出網易最新vod
-		foreach ( $html->find( '.lst-img-title li' ) as $index => $li ) {
-			if ( $index > 14 ) break;
-			$vod = $fetch_rows[$index];
-
-			preg_match( '@\/([\d]{2})\/([\d]{4})\/([\d]{2})\/@m', $li->find( 'a.pic', 0 )->href, $date );
-
-			$vod['date']  = "20$date[1]$date[2], $date[3]時";
-			// $vod['title'] = $code->chg_utfcode( iconv( "GB2312", "utf-8", $li->find( 'h5 a', 0 )->title ) );
-			$vod['title'] = iconv( "GB2312", "utf-8", $li->find( 'h5 a', 0 )->title );
-			$vod['title'] = str_replace( '暗黑破壞神3 ', '', $vod['title'] );
-			// $vod['text']  = $code->chg_utfcode( iconv( "GB2312", "utf-8", $li->find( 'h5 a', 0 )->innertext ) );
-			$vod['text']  = iconv( "GB2312", "utf-8", $li->find( 'h5 a', 0 )->innertext );
-			$vod['href']  = $li->find( 'h5 a', 0 )->href;
-
-			// 判斷網易的vod是否已存在於暗盟論壇
-			foreach ( $exisit_vod as $key => $entity ) {
-				$fetched = str_get_html( $entity['message'] );
-				// 轉文章時，於JS塞入的暗code
-				if ( $fetched->find( 'a#fetched', 0 )->href == $vod['href'] ) {
-					$vod['fetched']  = TRUE;
-					$vod['bbs_link'] = $this->discuzx->alink_to_bbs( array(
-							'text' => '已存在',
-							'tid'  => $entity['tid'],
-						) );
-				}
-				$fetched->clear();
-				unset( $fetched );
-			}
-
-			$fetch_rows[$index] = $vod;
-		}
-
-		$this->view->data['fetch_rows'] = $fetch_rows;
-		$this->view->display(
-			array(
-				'title'    => "網易新聞爬蟲 - 最近視頻",
-				'view'     => 'admin/news-bot/latest-vod',
-				'js_files' => array(
-					'admin/news-bot',
-				),
-				'css_files' => array(
-					'admin/diy',
-					'admin/news-bot'
-				),
-			)
-		);
+		$this->view->js_add( 'admin/live-channels' );
+		$this->view->css_add( 'admin/live-channels' );
 	}
 
 	/**
