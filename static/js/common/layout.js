882766b29ebe02a38a0f2f417b6c3dce4742ca36
lzzpnk <aiyswu@gmail.com>
2012-08-14 11:30:29 +0800

    綜合: 270*60廣告腳本驗證

--- static/js/common/layout.js
+++ static/js/common/layout.js
@@ -2,22 +2,33 @@ jQuery(function(){
 
 	var $ads = jQuery('#commercial_ad').find('.single');
 
-	var ad_amount = $ads.length - 1;
+	var ad_amount = $ads.length;
 
 	var random;
 
 	// 只顯示三個270x60廣告.
-	random = Math.round( Math.random() * ad_amount - 0 );
+	random = Math.floor( Math.random() * (ad_amount - 0) );
 	$ads.eq(random).show();
 	$ads.splice( random, 1 );
 
-	random = Math.round( Math.random() * ad_amount - 1 );
+	random = Math.floor( Math.random() * (ad_amount - 1) );
 	$ads.eq(random).show();
 	$ads.splice( random, 1 );
 
-	random = Math.round( Math.random() * ad_amount - 2 );
+	random = Math.floor( Math.random() * (ad_amount - 2) );
 	$ads.eq(random).show();
 	$ads.splice( random, 1 );
+
+	// // 驗證
+	// if ($ads.siblings('[href*="http://expendables2.catchplay.com/"]').is(':visible')) {
+	// 	console.log(/270x60 banner/, "浴血任務2");
+	// }
+	// if ($ads.siblings('[href*="http://www.cmstorm.com/tw/start/"]').is(':visible')) {
+	// 	console.log(/270x60 banner/, "CMSTORM");
+	// }
+	// if ($ads.siblings('[href*="http://www.roccat.org"]').is(':visible')) {
+	// 	console.log(/270x60 banner/, "ROCCAT");
+	// }
 
 	$ads.remove();
 });
